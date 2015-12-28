290b4d9f3749407d840c1c4c8006a30cb7e89190
Swifttote <Swifttote-AlphaMini@Swifttotes-Mac-mini.local>
2015-12-28 19:58:59 +0530

    Order placing working well

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	
 $('#addItemButton').click(function(){
 	var newItemHtml = $('.addItemBox').first().clone();
-	console.log(newItemHtml);
 	$('#itemsList').append(newItemHtml);
 });
 });
