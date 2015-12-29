ddf43fab498098067207ae3b710c9449975116b8
Swifttote <Swifttote-AlphaMini@Swifttotes-Mac-mini.local>
2015-12-29 19:50:40 +0530

    tuesay night

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,4 +4,18 @@ $('#addItemButton').click(function(){
 	var newItemHtml = $('.addItemBox').first().clone();
 	$('#itemsList').append(newItemHtml);
 });
+
+
 });
+function displayItem(itemsList,tableNo){
+	var panelElement = $('.hiddenOrder').first().clone();
+	panelElement.css('display','inherit');
+	var heading = panelElement.find('.panel-heading').html(tableNo);
+	console.log(heading);
+	for(var order in itemsList){
+			panelElement.find('.orderDetails').html(itemsList[order].item);
+			panelElement.find('.orderQnty').html(itemsList[order].quantity);
+            console.log(itemsList[order].quantity+" "+itemsList[order].item);
+          }
+
+}
