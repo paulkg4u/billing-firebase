40df9daa49c72b42a280d1aebd7de30fa813d5c0
Swifttote <Swifttote-AlphaMini@Swifttotes-Mac-mini.local>
2015-12-30 19:00:51 +0530

    table and kitchen working fine

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,18 +4,19 @@ $('#addItemButton').click(function(){
 	var newItemHtml = $('.addItemBox').first().clone();
 	$('#itemsList').append(newItemHtml);
 });
-
-
 });
-function displayItem(itemsList,tableNo){
-	var panelElement = $('.hiddenOrder').first().clone();
-	panelElement.css('display','inherit');
-	var heading = panelElement.find('.panel-heading').html(tableNo);
-	console.log(heading);
+function displayItem(itemsList,tableNo,initialElement){
+	var panelElement = initialElement.clone();
+	panelElement.css('display','inherit').css('font-size','1.2em');
+	var heading = panelElement.find('.panel-heading').html('<strong> Table #: '+tableNo+'</strong>');
+	panelElement.find('.orderDetails').html('');
+	panelElement.find('.orderQnty').html('');
+
 	for(var order in itemsList){
-			panelElement.find('.orderDetails').html(itemsList[order].item);
-			panelElement.find('.orderQnty').html(itemsList[order].quantity);
+			panelElement.find('.orderDetails').css('font-size','.9em').append($('<div/>').html(itemsList[order].item));
+			panelElement.find('.orderQnty').css('font-size','.9em').append($('<div/>').html(itemsList[order].quantity));
             console.log(itemsList[order].quantity+" "+itemsList[order].item);
           }
 
+    $('#pendingOrders').append(panelElement);
 }
